813fedb2c0a91595d95d25a2af9931a177f0d2be
Nishi Shah <nishi@blue-ocean-robotics.com>
2022-08-16 14:43:33 +0530

    constant file

---
+++ src/Utils/Constant.js
@@ -0,0 +1,18 @@
+export const ALL_POST_REQUEST = "ALL_POST_REQUEST";
+export const ALL_POST_SUCCESS = "ALL_POST_SUCCESS";
+export const ALL_POST_FAIL = "ALL_POST_FAIL";
+export const ADD_POST_REQUEST = "ADD_POST_REQUEST";
+export const ADD_POST_SUCCESS = "ADD_POST_SUCCESS";
+export const ADD_POST_FAIL = "ADD_POST_FAIL";
+export const UPDATE_POST_REQUEST = "UPDATE_POST_REQUEST";
+export const UPDATE_POST_SUCCESS = "UPDATE_POST_SUCCESS";
+export const UPDATE_POST_FAIL = "UPDATE_POST_FAIL";
+export const DELETE_POST_REQUEST = "DELETE_POST_REQUEST";
+export const DELETE_POST_SUCCESS = "DELETE_POST_SUCCESS";
+export const DELETE_POST_FAIL = "DELETE_POST_FAIL";
+
+export const ROUTES = {
+  HOME: "/",
+  LOGIN: "/login",
+  ADD_POST:"/add-post"
+};
